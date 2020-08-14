0e28f61fa3bdd437a314ed5db731e78f1a1dffc3
Bryan Chen <bryanchen74@gmail.com>
2020-08-14 06:03:50 -0400

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const fs = require("fs").promises;
+
+const app = express();
+app.use(express.json());
+app.use("/", express.static("../frontend/build"));
+
+app.get("/", async (req, res) => {
+  res.sendFile("../frontend/build/index.html", "utf-8");
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port 3000...");
+});
